dbd39d2c9bbcbccb835142e69ecb5685a1349d60
JayDee360 <67234094+jaydee360@users.noreply.github.com>
2025-07-28 16:45:03 +0100

    Add additional examples of list and map comprehensions in experiments2.sh

--- 01_TerraformTuesdays_ParsingDataWithForLoops/experiments2.sh
+++ 01_TerraformTuesdays_ParsingDataWithForLoops/experiments2.sh
@@ -9,3 +9,22 @@
 # 5. Output the results to verify the data is parsed correctly
 
 
+{
+    for food in distinct([for item in local.all_json_data.List3 : item.Food]) :
+    food => [
+      for item in local.all_json_data.List3 : lookup(item,[for k in keys(item) : k if startswith(k, "List3SubKey")][0],null)
+      if item.Food == food
+    ]
+}
+
+
+# How about a for loop on a list?
+[ for i in local.all_json_data.List1 : upper(i) ]
+
+# What about transposing keys and values?
+{ for key, val in local.map1_data : val => key }
+
+[ for k, v in local.map1_data : "${v}-${k}" ]
+
+# How about we get a list for only Tacos?
+[ for i in local.all_json_data.List3 : i if i.Food == "Taco"]
