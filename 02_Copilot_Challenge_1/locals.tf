134d647770795fc9134ee585d018d6b4f185f47b
JayDee360 <67234094+jaydee360@users.noreply.github.com>
2025-08-01 12:21:20 +0100

    define ingress / egress rules (local) - BAD version - with map of lists

--- 02_Copilot_Challenge_1/locals.tf
+++ 02_Copilot_Challenge_1/locals.tf
@@ -1,3 +1,4 @@
+/* 
 locals {
   ec2_data = jsondecode(file("ec2_data.json"))
 }
@@ -6,3 +7,12 @@ locals {
   eip_enabled = {for k, v in var.ec2instances : k => v if v.assign_eip == true}
 }
 
+ */
+
+locals {
+  ingress_rules = {for sg_key, sg_rule_type in var.ec2_security_group_rules : sg_key => flatten([for sg_rule_type_key, sg_rule in sg_rule_type : sg_rule if sg_rule_type_key == "ingress"])}
+}
+
+locals {
+  egress_rules = {for sg_key, sg_rule_type in var.ec2_security_group_rules : sg_key => flatten([for sg_rule_type_key, sg_rule in sg_rule_type : sg_rule if sg_rule_type_key == "egress"])}
+}
